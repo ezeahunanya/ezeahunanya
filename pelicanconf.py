8fb937150b642bc2299ec709c2a4a9d220e19188
Eze Ahunanya <ezeahunanya@outlook.com>
2020-12-05 09:19:27 +0000

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -49,8 +49,8 @@ DISPLAY_CATEGORIES_ON_MENU = False
 
 DELETE_OUTPUT_DIRECTORY = True
 
-ARTICLE_URL = 'blog/{date:%Y}/{slug}/'
-ARTICLE_SAVE_AS = 'blog/{date:%Y}/{slug}/index.html'
+ARTICLE_URL = 'blog/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
 PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
 
